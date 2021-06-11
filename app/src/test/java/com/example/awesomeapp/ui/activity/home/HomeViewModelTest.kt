a6c8cead52c54d6012f116cfe08ec01709340b47
guntur <laksamana.guntur24@gmail.com>
2021-06-11 20:22:06 +0700

    #master change HomeViewModelTest.kt

--- app/src/test/java/com/example/awesomeapp/ui/activity/home/HomeViewModelTest.kt
+++ app/src/test/java/com/example/awesomeapp/ui/activity/home/HomeViewModelTest.kt
@@ -2,12 +2,10 @@ package com.example.awesomeapp.ui.activity.home
 
 import com.example.awesomeapp.data.DataManager
 import com.example.awesomeapp.data.model.Response
-import com.example.awesomeapp.utils.rx.AppSchedulerProvider
 import com.example.awesomeapp.utils.rx.AppSchedulerProviderTest
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.TestScheduler
 import org.junit.After
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
@@ -24,34 +22,30 @@ class HomeViewModelTest {
     @Mock
     var dataManager: DataManager? = null
 
-    private var homeViewModel: HomeViewModel? = null
-    private var testScheduler: TestScheduler? = null
+    private lateinit var homeViewModel: HomeViewModel
+    private var testScheduler: TestScheduler = TestScheduler()
 
     @Before
     @Throws(Exception::class)
     fun setUp() {
-        testScheduler = TestScheduler()
-        val appSchedulerProvider = AppSchedulerProviderTest(testScheduler!!)
+        val appSchedulerProvider = AppSchedulerProviderTest(testScheduler)
         homeViewModel = dataManager?.let { HomeViewModel(it, appSchedulerProvider) }!!
-        homeInterface?.let { homeViewModel?.setNavigator(it) }
+        homeInterface?.let { homeViewModel.setNavigator(it) }
     }
 
     @After
     @Throws(Exception::class)
     fun tearDown() {
-        testScheduler = null
-        homeViewModel = null
         homeInterface = null
     }
 
     @Test
-    fun testServerLoginSuccess() {
+    fun testServer() {
         val response = Response()
         doReturn(Single.just(response))
-            .`when`(dataManager)
-            ?.getCurated(1, 2)
-        homeViewModel?.loadData(1)
-        testScheduler?.triggerActions()
+            .`when`(dataManager)?.getCurated(1, 2)
+        homeViewModel.loadData(1)
+        testScheduler.triggerActions()
 
         verify(homeInterface)?.setTotalResult(1)
     }
